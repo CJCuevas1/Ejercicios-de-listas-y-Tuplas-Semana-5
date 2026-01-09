c7692db644390530bb4d965fde2ffa46b73625f3
CJCuevas1 <cj.cuevas@uea.edu.ec>
2026-01-09 16:50:04 -0500

    Add files via upload

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio5
+{
+    class Numeros
+    {
+        private List<int> numeros;
+
+        public Numeros()
+        {
+            numeros = new List<int>();
+
+            // Guardamos los números del 1 al 10
+            for (int i = 1; i <= 10; i++)
+            {
+                numeros.Add(i);
+            }
+        }
+
+        // Método para mostrar los números en orden inverso
+        public void MostrarInverso()
+        {
+            for (int i = numeros.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.Write(numeros[i]);
+                }
+                else
+                {
+                    Console.Write(numeros[i] + ", ");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros numeros = new Numeros();
+            numeros.MostrarInverso();
+
+            Console.ReadKey();
+        }
+    }
+}
