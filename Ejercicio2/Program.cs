9544b42f4d839b8a2fb71dfd9f45cbd35d643722
CJCuevas1 <cj.cuevas@uea.edu.ec>
2026-01-09 16:46:10 -0500

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio2
+{
+    class Curso
+    {
+        // Lista de asignaturas
+        private List<string> asignaturas;
+
+        // Constructor
+        public Curso()
+        {
+            asignaturas = new List<string>();
+
+            asignaturas.Add("Matemáticas");
+            asignaturas.Add("Física");
+            asignaturas.Add("Química");
+            asignaturas.Add("Historia");
+            asignaturas.Add("Lengua");
+        }
+
+        // Método para mostrar el mensaje
+        public void MostrarAsignaturas()
+        {
+            foreach (string asignatura in asignaturas)
+            {
+                Console.WriteLine("Yo estudio " + asignatura);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso curso = new Curso();
+            curso.MostrarAsignaturas();
+
+            Console.ReadKey();
+        }
+    }
+}
