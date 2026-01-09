2aed61cd4cb0e2cd05dde97dd3a3ad5ea77554d1
CJCuevas1 <cj.cuevas@uea.edu.ec>
2026-01-09 16:47:07 -0500

    Add files via upload

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio3
+{
+    class Curso
+    {
+        // Lista de asignaturas
+        private List<string> asignaturas;
+
+        // Lista de notas
+        private List<double> notas;
+
+        // Constructor
+        public Curso()
+        {
+            asignaturas = new List<string>();
+            notas = new List<double>();
+
+            asignaturas.Add("Matemáticas");
+            asignaturas.Add("Física");
+            asignaturas.Add("Química");
+            asignaturas.Add("Historia");
+            asignaturas.Add("Lengua");
+        }
+
+        // Método para pedir las notas
+        public void PedirNotas()
+        {
+            for (int i = 0; i < asignaturas.Count; i++)
+            {
+                Console.Write("Ingrese la nota de " + asignaturas[i] + ": ");
+                double nota = Convert.ToDouble(Console.ReadLine());
+                notas.Add(nota);
+            }
+        }
+
+        // Método para mostrar las notas
+        public void MostrarNotas()
+        {
+            Console.WriteLine("\nResultados:");
+
+            for (int i = 0; i < asignaturas.Count; i++)
+            {
+                Console.WriteLine("En " + asignaturas[i] + " has sacado " + notas[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso curso = new Curso();
+
+            curso.PedirNotas();
+            curso.MostrarNotas();
+
+            Console.ReadKey();
+        }
+    }
+}
